18359c37c91801767efbad4af3c81a2f96f131d9
Tatiana Leitao <tatiana_felisberto_@hotmail.com>
2023-01-28 14:57:30 +0000

    -added div forecast

--- assets/script.js
+++ assets/script.js
@@ -15,9 +15,6 @@ function getCity(e) {
   } else {
     console.log("Hello world");
   }
-  // Here I'm building an URL that we need to query the database
-
-  // created a div to add my elements inside
 }
 // added evente in a search button
 $("#search-button").on("click", getCity);
@@ -51,12 +48,25 @@ function getCurrWeather(lat, lon) {
 
 function displayCurrWeather(weather) {
   var todaySection = $("#today");
+  var forecastSection = $("#forecast");
 
+  // section today, I create a dinamically div
   var card = $("<div>")
     .addClass("border border-secondary  card ")
     .css({ width: "60rem", height: "10rem" });
 
+  // section forcast, I created other section for focast
+  var cardEl = $("<div>")
+    .addClass("border border-secondary  card ")
+    .css({ width: "60rem", height: "10rem" });
+
+  // forercast section will append to the cardEl
+  forecastSection.append(cardEl);
+
+  // tofay section will append to the card
   todaySection.append(card);
+
+  // icon section
   var icon = $("<img>");
   icon.attr(
     "scr",
@@ -66,33 +76,59 @@ function displayCurrWeather(weather) {
 
   //icon.css({ width: "200px", height: "200px", "object-fit": "contain" });
 
+  //Today section
+  // created a ul list, so that i can storage the weather informattion like city, wind, temp etc..
   var list = $("<ul>").addClass("list-unstyled pl-5");
-
+  //this list will append to a today card
   card.append(list);
 
+  // city
   var cityItem = $("<li>").addClass("h2");
+  // fetch from the API the city user will saerch
   var city = cityQuery;
+  // Will show the city
   cityItem.text(city);
 
+  //temperature
   var tempItem = $("<li>");
+  // fech from the API the temperature
   var celsiusTemperature = weather.current.temp;
+  // will show the temperature in celsios
   tempItem.text("Temp: " + celsiusTemperature + "°C");
 
+  // wind
   var windItem = $("<li>");
+  // fech from the API the wind
   var wind = weather.current.wind_speed;
+  // will show the temperatue in m/s
   windItem.text("wind: " + wind + " m/s");
 
+  // humidity
   var humidityItem = $("<li>");
+  // fech from the API the humidity
   var humidity = weather.current.humidity;
+  // will show the humidity in %
   humidityItem.text("humidity: " + humidity + "%");
   // console.log(humidity);
 
-  cityItem.append(icon);
-
+  // this part will make sure all of the list will show in the screen
   list.append(cityItem);
+  list.append(icon);
   list.append(tempItem);
-
   list.append(windItem);
-
   list.append(humidityItem);
+
+  //Forecast Div
 }
+
+// function DisplayFiveDays(daily) {
+//   //html id forecats
+//   var dailyWeather = $("#forecast");
+
+//   //created a bootstrap card and after I append to the forecast ID
+//   var cardEl = $("<div>");
+//   // .addClass("card bg-info")
+//   // .css({ width: "18rem", height: "20rem" });
+
+//   dailyWeather.append(cardEl);
+// }
